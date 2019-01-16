045b6ba7ab7cf5f3c57205d04def4aae82e8d34c
cclion <cclion0088@gmail.com>
2019-01-16 16:58:26 +0800

    添加swift OC桥接文件

---
+++ CCSwift/Main/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  CCSwift
+//
+//  Created by job on 2019/1/16.
+//  Copyright © 2019 我是花轮. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FLEX/FLEX.h>
+
+#endif /* Bridging_Header_h */
